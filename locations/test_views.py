b0543f94fffdb824ea72dbea47f6d81a7d82dc6e
Jared Newell <jared.newell@gmail.com>
2015-04-04 23:48:12 -0700

    refactoring of resolver tests into test_all_views_resolve

--- locations/test_views.py
+++ locations/test_views.py
@@ -21,11 +21,22 @@ class TestLocationsViews(TestCase):
         found = resolve('/admin/')
         self.assertEqual(found.app_name, 'admin')
 
-    def test_datacenter_view_url(self):
-        url = reverse('locations.views.datacenter_create')
-        response = resolve(url)
-        self.assertEqual(response.url_name, 'locations_datacenter_create')
-        self.assertEqual(response.view_name, 'locations_datacenter_create')
+    def test_all_views_resolve(self):
+        #tuple of View name, URL name to be tested against.  A resolve on the reverse of the view should result in the URL name.
+        view_list = (('locations.views.datacenter_create', 'locations_datacenter_create'),
+                     ('locations.views.cage_create', 'locations_cage_create'),
+                     ('locations.views.cagerow_create', 'locations_cagerow_create'),
+                     ('locations.views.rack_create', 'locations_rack_create'),
+                     ('locations.views.datacenter_by_id', 'locations_datacenter_view_by_id'),
+                     ('locations.views.cage_by_id', 'locations_cage_view_by_id'),
+                     ('locations.views.cagerow_by_id', 'locations_cagerow_view_by_id'),
+                     ('locations.views.rack_by_id', 'locations_rack_view_by_id'))
+
+        for item in view_list:
+            url = reverse(item[0])
+            response = resolve(url)
+            self.assertEqual(response.url_name, item[1])
+            self.assertEqual(response.view_name, item[1])
 
     def test_datacenter_view_get(self):
         url = reverse('locations.views.datacenter_create')
@@ -38,33 +49,15 @@ class TestLocationsViews(TestCase):
         response = c.post(url, dict(name='test datacenter', description=' a test datacenter'))
         print dir(response)
 
-    def test_cage_view_url(self):
-        url = reverse('locations.views.cage_create')
-        response = resolve(url)
-        self.assertEqual(response.url_name, 'locations_cage_create')
-        self.assertEqual(response.view_name, 'locations_cage_create')
-
     def test_cage_view_get(self):
         url = reverse('locations.views.cage_create')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_cagerow_view_url(self):
-        url = reverse('locations.views.cagerow_create')
-        response = resolve(url)
-        self.assertEqual(response.url_name, 'locations_cagerow_create')
-        self.assertEqual(response.view_name, 'locations_cagerow_create')
-
     def test_cagerow_view_get(self):
         url = reverse('locations.views.cagerow_create')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-
-    def test_rack_view_url(self):
-        url = reverse('locations.views.rack_create')
-        response = resolve(url)
-        self.assertEqual(response.url_name, 'locations_rack_create')
-        self.assertEqual(response.view_name, 'locations_rack_create')
 
     def test_rack_view_get(self):
         url = reverse('locations.views.rack_create')
