4453b912cf8d0a79d95cb32fbc0ed7a291a3941b
Jared Newell <jared.newell@gmail.com>
2015-04-05 00:14:17 -0700

    some more test refactoring

--- locations/test_views.py
+++ locations/test_views.py
@@ -22,17 +22,19 @@ class TestLocationsViews(TestCase):
         self.assertEqual(found.app_name, 'admin')
 
     def test_all_views_resolve(self):
-        #tuple of View name, URL name to be tested against.  A resolve on the reverse of the view should result in the URL name.
+        #tuple of tuples... (View name, URL name to be tested against).  A resolve on the reverse of the view name should result in the URL name.
         view_list = (('locations.views.datacenter_create', 'locations_datacenter_create'),
                      ('locations.views.cage_create', 'locations_cage_create'),
                      ('locations.views.cagerow_create', 'locations_cagerow_create'),
-                     ('locations.views.rack_create', 'locations_rack_create'),
-                     ('locations.views.datacenter_by_id', 'locations_datacenter_view_by_id'),
-                     ('locations.views.cage_by_id', 'locations_cage_view_by_id'),
-                     ('locations.views.cagerow_by_id', 'locations_cagerow_view_by_id'),
-                     ('locations.views.rack_by_id', 'locations_rack_view_by_id'))
+                     ('locations.views.rack_create', 'locations_rack_create'),)
+                    #todo: the views below require arguments...  need to fix the function to send arguments to test these views
+                     # ('locations.views.datacenter_by_id', 'locations_datacenter_view_by_id'),
+                     # ('locations.views.cage_by_id', 'locations_cage_view_by_id'),
+                     # ('locations.views.cagerow_by_id', 'locations_cagerow_view_by_id'),
+                     # ('locations.views.rack_by_id', 'locations_rack_view_by_id'))
 
         for item in view_list:
+            print "testing ", item
             url = reverse(item[0])
             response = resolve(url)
             self.assertEqual(response.url_name, item[1])
@@ -43,23 +45,19 @@ class TestLocationsViews(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
+    def test_all_views_get(self):
+        view_list = ('locations.views.datacenter_create', 'locations.views.cage_create', 'locations.views.cagerow_create',
+                     'locations.views.rack_create')
+
+        for item in view_list:
+            print "testing ", item
+            url = reverse(item)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+
     def test_datacenter_view_post_create_datacenter(self):
         url = reverse('locations.views.datacenter_create')
         c = Client()
         response = c.post(url, dict(name='test datacenter', description=' a test datacenter'))
-        print dir(response)
-
-    def test_cage_view_get(self):
-        url = reverse('locations.views.cage_create')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_cagerow_view_get(self):
-        url = reverse('locations.views.cagerow_create')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_rack_view_get(self):
-        url = reverse('locations.views.rack_create')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        #todo actually do an assertion here....
+        print response.status_code
