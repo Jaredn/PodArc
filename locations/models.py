562b8512452239c1d79b909b0a04c3b0e00cd066
Jared Newell <jared.newell@gmail.com>
2015-04-07 16:18:04 -0700

    removed max_length from integer field models.

--- locations/models.py
+++ locations/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Datacenters(models.Model):
     name = models.CharField(max_length="64", unique=True)
     description = models.CharField(max_length="255")
-    active = models.IntegerField(max_length="1", default=1)
+    active = models.IntegerField(default=1)
 
     def __str__(self):
         return "%s" % (self.name)
@@ -16,7 +16,7 @@ class Cages(models.Model):
     name = models.CharField(max_length="64")
     datacenter = models.ForeignKey(Datacenters) #Links to Datacenters.id
     description = models.CharField(max_length="255")
-    active = models.IntegerField(max_length="1", default=1)
+    active = models.IntegerField(default=1)
 
     def __str__(self):
         return "%s.%s" % (self.name, self.datacenter.name)
@@ -29,7 +29,7 @@ class CageRows(models.Model):
     name = models.CharField(max_length="64")
     cage = models.ForeignKey(Cages) #Links to Cages.id
     description = models.CharField(max_length="255")
-    active = models.IntegerField(max_length="1", default=1)
+    active = models.IntegerField(default=1)
 
     def __str__(self):
         return "%s.%s.%s" % (self.name, self.cage.name, self.cage.datacenter.name)
@@ -42,7 +42,7 @@ class Racks(models.Model):
     name = models.CharField(max_length="64")
     cagerow = models.ForeignKey(CageRows) #Links to CageRows.id
     description = models.CharField(max_length="255")
-    active = models.IntegerField(max_length="1", default=1)
+    active = models.IntegerField(default=1)
 
     def __str__(self):
         return "%s.%s.%s.%s" % (self.name, self.cagerow.name, self.cagerow.cage.name, self.cagerow.cage.datacenter.name)
